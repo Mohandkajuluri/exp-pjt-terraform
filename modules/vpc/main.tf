793c2db8037ad1340556ab166c2e09806cb3f221
Mohandkajuluri <md.kajuluri@gmail.com>
2024-01-24 18:09:25 -0500

    exp vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,7 +7,7 @@ resource "aws_subnet" "public" {
   count      = length(var.public_subnets)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets[count.index]
-  tags       = merge(var.tags, {Name = public_subnets})
+  tags       = merge(var.tags, { Name = "public_subnets" })
 
 
 }

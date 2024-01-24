278012dac7b92140c7862441a2806c64331c99a6
Mohandkajuluri <md.kajuluri@gmail.com>
2024-01-24 18:06:33 -0500

    exp vpc

--- main.tf
+++ main.tf
@@ -3,4 +3,8 @@ module "vpc" {
   vpc_cidr_block = var.vpc_cidr_block
   tags           = var.tags
   env            = var.env
+  public_subnets = var.public_subnets
+  web_subnets    = var.web_subnets
+  app_subnets    = var.app_subnets
+  db_subnets     = var.db_subnets
 }

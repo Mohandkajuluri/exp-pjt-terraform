3f5eb781ddfb1648167a36e3bfe30aac2f04dbb7
Mohandkajuluri <md.kajuluri@gmail.com>
2024-02-02 21:29:43 -0500

    rds

--- main.tf
+++ main.tf
@@ -28,3 +28,16 @@ module "rds" {
 #  kms = var.kms
 }
 
+module "app" {
+  source = "./modules/app"
+  app_port       = "var.backend_app_port"
+  component      = "backend"
+  env            = "var.env"
+  instance_count = var.backend["instance_count"]
+  instance_type  = var.backend["instance_type"]
+  sg_cidrs       = var.web_subnets
+  subnets        = module.vpc.app_subnets
+  tags           = var.tags
+  vpc_id         = module.vpc.vpc_id
+}
+

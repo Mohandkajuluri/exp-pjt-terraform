3eba3be2b09ab3aa71efdc389fa5bcbda932ae68
Mohandkajuluri <md.kajuluri@gmail.com>
2024-02-08 14:50:52 -0500

    bastion

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,15 +1,15 @@
-resource "aws_security_group" "main" {
-  name        = "${var.env}-${var.component}"
-  description = "${var.env}-${var.component}"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "app"
-    from_port   = var.app_port
-    to_port     = var.app_port
-    protocol    = "tcp"
-    cidr_blocks = var.sg_cidrs
-  }
+#resource "aws_security_group" "main" {
+#  name        = "${var.env}-${var.component}"
+#  description = "${var.env}-${var.component}"
+#  vpc_id      = var.vpc_id
+#
+#  ingress {
+#    description = "app"
+#    from_port   = var.app_port
+#    to_port     = var.app_port
+#    protocol    = "tcp"
+#    cidr_blocks = var.sg_cidrs
+#  }
 
   ingress {
     description = "ssh"
@@ -19,13 +19,13 @@ resource "aws_security_group" "main" {
     cidr_blocks = var.bastion_cidrs
   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
 
   tags = merge(var.tags, { Name = "${var.env}-${var.component}" })
 }

ec956fc6f72e6488985d8abe68f9d9d983b306d8
Mohandkajuluri <md.kajuluri@gmail.com>
2024-02-08 14:45:35 -0500

    bastion

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "main" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = bastion_cidrs
+    cidr_blocks = var.bastion_cidrs
   }
 
   egress {
